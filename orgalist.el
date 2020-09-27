48fa839f9120f95b39edd41dc3b46a61d393abe6
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2020-09-27 12:13:27 +0200

    Bump to version 1.13

--- orgalist.el
+++ orgalist.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24.4"))
-;; Version: 1.12
+;; Version: 1.13
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
