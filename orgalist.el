6b97873be78dba574f4753104c8f2da3c8bfb5ee
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-06-17 20:05:47 +0200

    orgalist: Release 1.15.
    * orgalist.el: Bump version to 1.15.

--- orgalist.el
+++ orgalist.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1"))
-;; Version: 1.14
+;; Version: 1.15
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
